f7712b2eace65172c153261e4d2c747915c3c4cc
Ramesh Boopathi <sanjai.ramesh@gmail.com>
2026-03-08 00:13:16 +0530

    add new method to calculate minimum flips for alternating binary strings

--- slidingwindow/MinFlipsMakeBinaryStrAlternating.java
+++ slidingwindow/MinFlipsMakeBinaryStrAlternating.java
@@ -65,8 +65,35 @@ public class MinFlipsMakeBinaryStrAlternating {
         return ans;
     }
 
+    public int minFlip1(String s) {
+
+        int n = s.length();
+        int res = n;
+
+        int op[] = {0,0};
+        /*
+        111000
+
+        010101
+        101010
+         */
+
+        for( int i=0;i<n;i++){
+
+            op[(s.charAt(i)^i) & 1 ]++;
+        }
+
+        for( int i=0;i<n;i++){
+            op[(s.charAt(i)^i) & 1]--;
+            op[(s.charAt(i)^(n+i)) & 1]++;
+            res = Math.min(res, Math.min(op[0],op[1]));
+        }
+        return res;
+    }
+
     public static void main(String[] args) {
         MinFlipsMakeBinaryStrAlternating obj = new MinFlipsMakeBinaryStrAlternating();
         System.out.println(obj.minFlip("111000"));
+        System.out.println(obj.minFlip1("010"));
     }
 }
